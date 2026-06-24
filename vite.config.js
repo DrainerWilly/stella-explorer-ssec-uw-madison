f361f4e80ec676366faa7ed38c60513d18a87633
DrainerWilly <mkaraasl@u.rochester.edu>
2026-06-24 15:22:47 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/SSEC-Dev/",
+  base: "/stella-explorer-ssec-uw-madison/",
   plugins: [react()],
 });
